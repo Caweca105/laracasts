1d81b521ccd210a99fb8bce958903108000b7ce6
Diogo Lemos <lemos.diogo105@gmail.com>
2023-05-22 01:40:30 +0100

    Learning new things about JS

---
+++ 1. Modern Javascript Basics/episode 1/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var username = {
+    name: 'Diogo',
+    email: 'diogo@example.com'
+}
+
+function init() {
+    // if(true) {
+    //     var email = 'diogo@example.com';
+    //     let email = 'diogo@example.com';
+
+    //     document.getElementById('output').innerHTML = email;
+    // }
+
+    document.getElementById('output').innerHTML = username;
+
+    // var email = 'diogo@example.com';
+}
+
+init()
